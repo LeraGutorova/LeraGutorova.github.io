8f4b67563df7aa80b6d01e398f8ed19220f2506d
LeraGutorova <valeryiagutorova@vk.com>
2022-01-10 11:53:37 +0300

    page_1+

---
+++ basket.js
@@ -0,0 +1,28 @@
+const tours = [{
+        namePlace: 'Львов 2 ночи + Карпаты',
+        price: '262 BYN',
+    },
+    {
+        namePlace: 'Новый год в Питере + Карелия',
+        price: '426 BYN',
+    },
+    {
+        namePlace: 'Новый год во Львове + Карпаты',
+        price: '424 BYN',
+    },
+    {
+        namePlace: 'Львов 1 ноч + Карпаты',
+        price: '162 BYN',
+    },
+
+]
+let basketElement = document.querySelector('.basket');
+
+function addTurLviv() {
+    let tourName = tours[1].namePlace;
+    let tourPrice = tours[1].price;
+    let html = '';
+    html += ``
+
+
+}
